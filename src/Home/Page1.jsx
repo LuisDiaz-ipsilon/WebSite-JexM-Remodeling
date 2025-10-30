36e9fe1014e9e2b0db07aa48414e76369dfde475
LuisDiaz-ipsilon <54384617+LuisDiaz-ipsilon@users.noreply.github.com>
2025-10-29 23:24:33 -0600

    feat gallery

--- src/Home/Page1.jsx
+++ src/Home/Page1.jsx
@@ -7,32 +7,50 @@ import photo4 from './static/images/photo4.png';
 import photo5 from './static/images/photo5.png';
 import photo6 from './static/images/photo6.png';
 
-
 export default function Page1() {
+  //const navigate = useNavigate();
+
   const topics = [
-    'The kitchen cabinets', 'Painting Interior', 'Ceramic', 'Doors installment',
-    'Baseborads',
+    { title: 'The Kitchen Cabinets', image: photo1 },
+    { title: 'Painting Interior', image: photo2 },
+    { title: 'Ceramic', image: photo3 },
+    { title: 'Doors Installment', image: photo4 },
+    { title: 'Baseboards', image: photo5 },
+    { title: 'Floors', image: photo6 },
   ];
 
-  const images = [photo1, photo2, photo3, photo4, photo5, photo6];
-
+  const handleClick = (topic) => {
+    console.log('Redirigir');
+    //navigate(`/topic/${encodeURIComponent(topic.title)}`);
+  };
 
   return (
     <div className="gallery-wrapper">
       <div className="gallery-grid">
-        {images.map((src, i) => (
-          <div className="gallery-item" key={i}>
-            <img src={src} alt={`scene-${i}`} />
+        {topics.map((topic, i) => (
+          <div
+            className="gallery-item"
+            key={i}
+            onClick={() => handleClick(topic)}
+          >
+            <img src={topic.image} alt={topic.title} />
+            <div className="overlay">
+              <span className="overlay-text">{topic.title}</span>
+            </div>
           </div>
         ))}
       </div>
 
       <aside className="sidebar">
-        <h1 className="brand">WE ARE   <span>EXPERTS</span></h1>
+        <h1 className="brand">
+          WE ARE <span>EXPERTS</span>
+        </h1>
         <h2 className="title">WORKS</h2>
         <ul className="locations">
-          {topics.map(loc => (
-            <li key={loc}>{loc}</li>
+          {topics.map((topic) => (
+            <li key={topic.title} onClick={() => handleClick(topic)}>
+              {topic.title}
+            </li>
           ))}
         </ul>
       </aside>
